de54e54787d1d1c3abe26fb45facd8ed8b023299
Mustafa Ahmed Azar <mustafa.azar04@gmail.com>
2026-01-21 17:21:12 +0300

    Collisions
    Added Collisions, removed adaptive performance package, added scoring

--- CubeController-main/Assets/Scripts/SurferMovement.cs
+++ CubeController-main/Assets/Scripts/SurferMovement.cs
@@ -2,63 +2,74 @@ using DG.Tweening;
 using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.SocialPlatforms.Impl;
 using UnityEngine.UIElements;
 
 public class SurferMovement : MonoBehaviour
 {
     public float cubeSpeed = 8.0f;
     bool L, M, R, Jumped;
+    public bool Alive;
+    public int score = 0;
 
     void Start()
     {
+        Application.targetFrameRate = 60;
         L = false;
         R = false;
         M = true;
         Jumped = false;
+        Alive = true;
     }
 
     void Update()
     {
         transform.Translate(new Vector3(0, 0, cubeSpeed) * Time.deltaTime);
 
+        if (Alive == true && cubeSpeed > 3)
+        {
 
-        if (Keyboard.current.aKey.wasPressedThisFrame)
-        {
-            if (L == false && M == true)
+            if (Keyboard.current.aKey.wasPressedThisFrame)
             {
-                transform.DOMoveX(-1.75f, 0.3f);
-                L = true;
-                M = false;
+                if (L == false && M == true)
+                {
+                    transform.DOMoveX(-1.75f, 0.3f);
+                    L = true;
+                    M = false;
+                }
+                else if (R == true)
+                {
+                    transform.DOMoveX(0, 0.3f);
+                    L = false;
+                    M = true;
+                    R = false;
+                }
             }
-            else if (R == true)
-            {
-                transform.DOMoveX(0, 0.3f);
-                L = false;
-                M = true;
-                R = false;
-            }
-        }
 
-        if (Keyboard.current.dKey.wasPressedThisFrame)
-        {
-            if (R == false && M == true)
+            if (Keyboard.current.dKey.wasPressedThisFrame)
             {
-                transform.DOMoveX(1.75f, 0.3f);
-                R = true;
-                M = false;
+                if (R == false && M == true)
+                {
+                    transform.DOMoveX(1.75f, 0.3f);
+                    R = true;
+                    M = false;
+                }
+                else if (L == true)
+                {
+                    transform.DOMoveX(0, 0.3f);
+                    R = false;
+                    M = true;
+                    L = false;
+                }
             }
-            else if (L == true)
+            if (Keyboard.current.wKey.wasPressedThisFrame && Jumped == false)
             {
-                transform.DOMoveX(0, 0.3f);
-                R = false;
-                M = true;
-                L = false;
+                Jumped = true;
+                StartCoroutine(Delay());
             }
-        }
-        if (Keyboard.current.wKey.wasPressedThisFrame && Jumped == false)
+        } else
         {
-            Jumped = true;
-            StartCoroutine(Delay());
+            finish();
         }
     }
 
@@ -70,5 +81,35 @@ public class SurferMovement : MonoBehaviour
         yield return new WaitForSeconds(0.2f);
         Jumped = false;
 
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Finish"))
+        {
+            finish();
+        }
+        if (other.CompareTag("SlowDown"))
+        {
+            cubeSpeed -= 0.5f;
+            if (score > 0)
+            {
+                score--;
+            }
+            Destroy(other.gameObject);
+        }
+        if (other.CompareTag("SpeedUp"))
+        {
+            cubeSpeed += 0.5f;
+            score++;
+            Destroy(other.gameObject);
+        }
+    }
+
+    void finish()
+    {
+        cubeSpeed = 0;
+        Alive = false;
+        print("Game Over! Your score is: " + score);
     }
 }
